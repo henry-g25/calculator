94193f24def8007a7fa992990ee8f64ae0dcbb38
henry-g25 <63613567+henry-g25@users.noreply.github.com>
2022-06-15 15:30:29 -0400

    Add files via upload

---
+++ src/test/java/com/henryg25/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henryg25.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
